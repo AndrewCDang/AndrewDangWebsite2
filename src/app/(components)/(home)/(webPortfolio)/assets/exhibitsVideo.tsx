780816df697397beadf7803661c245c875ef14f5
Andrew Dang <andrew-dang2@hotmail.co.uk>
2024-12-20 01:47:45 +0000

    fixed mobile video for exhibits

--- src/app/(components)/(home)/(webPortfolio)/assets/exhibitsVideo.tsx
+++ src/app/(components)/(home)/(webPortfolio)/assets/exhibitsVideo.tsx
@@ -12,20 +12,24 @@ function ExhibitsVideo() {
         <>
             {isClient && (
                 <video
-                    className={style.videoStyle}
                     style={{ width: "100%", aspectRatio: "1" }}
+                    className={style.videoStyle}
                     autoPlay
                     loop
                     muted
                     playsInline
                 >
                     <source
-                        media="(min-width: 1200px)"
-                        src={"/homepage/241219_Exhibits_video_1080p.mp4"}
+                        media="(min-width:1024px)"
+                        src="/homepage/241219_Exhibits_video_1080p.mp4"
                         type="video/mp4"
                     />
                     <source
-                        src={"/homepage/241219_Exhibits_video_480p.mp4"}
+                        src="/homepage/241219_Exhibits_video_480p.mp4"
+                        type="video/mp4"
+                    />
+                    <source
+                        src={"/homepage/flashmu_video.mp4"}
                         type="video/mp4"
                     />
                     Your browser does not support the video tag.
